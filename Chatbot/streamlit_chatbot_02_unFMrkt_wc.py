49deb428293e61808efdd8ae85381292783ca72c
VarpasConcepts <engineering@varpas.in>
2024-10-08 13:20:33 +0530

    Update streamlit_chatbot_02_unFMrkt_wc.py

--- Chatbot/streamlit_chatbot_02_unFMrkt_wc.py
+++ Chatbot/streamlit_chatbot_02_unFMrkt_wc.py
@@ -621,7 +621,7 @@ def main():
             /* Root variables */
             :root {
                 --secondary-background-color: #f0f2f6;
-                font-size: 18px;
+                font-size: 40px;
             }
 
             [data-testid="stSidebar"] [data-testid="stSidebarNav"] button[kind="header"] {
